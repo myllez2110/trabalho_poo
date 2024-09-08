5fce27108a3e6b7b2b5e975c999eebcdd8d68b40
myllez2110 <bapniel21@gmail.com>
2024-09-08 12:15:22 -0300

    Classes aluno e pessoa

--- app/src/main/java/trabalho_poo/app/Aluno.java
+++ app/src/main/java/trabalho_poo/app/Aluno.java
@@ -1,5 +1,30 @@
 package trabalho_poo.app;
 
-public class Aluno {
+import java.util.List;
 
+public class Aluno extends Pessoa {
+    private String matricula;
+    private List<Disciplina> disciplinas;
+
+    public Aluno(String nome, String cpf, String matricula) {
+        super(nome, cpf);
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public List<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(List<Disciplina> disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Matrícula: " + matricula;
+    }
 }
--- app/src/main/java/trabalho_poo/app/Pessoa.java
+++ app/src/main/java/trabalho_poo/app/Pessoa.java
@@ -1,5 +1,24 @@
 package trabalho_poo.app;
 
-public class Pessoa {
+public abstract class Pessoa {
+    private String nome;
+    private String cpf;
 
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", CPF: " + cpf;
+    }
 }
